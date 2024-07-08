4d0fee2c21984b2034eddd825b0f7cc781ef293e
Reza Aditya <rezaaditya@Rezas-MacBook-Pro.local>
2024-07-09 05:23:36 +0700

    report: config coverage with istanbul

--- test-config/karma.conf.js
+++ test-config/karma.conf.js
@@ -1,61 +1,82 @@
-var webpackConfig = require('./webpack.test.js');
+var webpackConfig = require("./webpack.test.js");
 
-module.exports = function(config) {
+module.exports = function (config) {
   var _config = {
-    basePath: '../',
-
-    frameworks: ['jasmine'],
-
+    basePath: "../",
+    frameworks: ["jasmine"],
     files: [
       {
-        pattern: './test-config/karma-test-shim.js',
-        watched: true
+        pattern: "./test-config/karma-test-shim.js",
+        watched: true,
       },
       {
-        pattern: './src/assets/**/*',
+        pattern: "./src/assets/**/*",
         watched: false,
         included: false,
         served: true,
-        nocache: false
-      }
+        nocache: false,
+      },
     ],
-
     proxies: {
-      '/assets/': '/base/src/assets/'
+      "/assets/": "/base/src/assets/",
     },
-
     preprocessors: {
-      './test-config/karma-test-shim.js': ['webpack', 'sourcemap']
+      "./test-config/karma-test-shim.js": ["webpack", "sourcemap"],
     },
-
     webpack: webpackConfig,
-
     webpackMiddleware: {
-      stats: 'errors-only'
+      stats: "errors-only",
     },
-
     webpackServer: {
-      noInfo: true
+      noInfo: true,
     },
-
     browserConsoleLogOptions: {
-      level: 'log',
-      format: '%b %T: %m',
-      terminal: true
+      level: "log",
+      format: "%b %T: %m",
+      terminal: true,
     },
-
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
+      reports: ["text-summary", "html", "lcovonly"],
+      fixWebpackSourcePaths: true,
+      combineBrowserReports: true,
+      skipFilesWithNoCoverage: true,
+      "report-config": {
+        html: {
+          subdir: "html",
+        },
+      },
+      thresholds: {
+        emitWarning: false,
+        global: {
+          statements: 85,
+          lines: 85,
+          branches: 85,
+          functions: 85,
+        },
+        each: {
+          statements: 85,
+          lines: 85,
+          branches: 85,
+          functions: 85,
+          overrides: {
+            "baz/component/**/*.js": {
+              statements: 98,
+            },
+          },
+        },
+      },
+      // output config used by istanbul for debugging
+      verbose: true,
     },
-
-    reporters: config.coverage ? ['kjhtml', 'dots', 'coverage-istanbul'] : ['kjhtml', 'dots'],
+    reporters: config.coverage
+      ? ["kjhtml", "progress", "coverage-istanbul"]
+      : ["kjhtml", "dots"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ["Chrome"],
+    singleRun: false,
   };
 
   config.set(_config);
